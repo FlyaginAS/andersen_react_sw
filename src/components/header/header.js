8819e235b8c81539a3b6fc54f67613fb1a04922d
FlyaginAS <rus.reg.36@gmail.com>
2021-10-03 18:28:44 +0300

    лого- добавил атрибут тайтл. перевел на линк

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../button/button';
 import './header.css';
 import logo from './logo.png';
@@ -6,9 +7,9 @@ import logo from './logo.png';
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__logo">
-        <img src={logo} alt="Logo" />
-      </div>
+      <Link to="/" className="header__logo">
+        <img src={logo} alt="Logo" title="To Home Page" />
+      </Link>
       <div className="header__buttons">
         <Button label="Вход" />
         <Button label="Регистрация" />
