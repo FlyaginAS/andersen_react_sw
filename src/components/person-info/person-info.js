243eddc2f5a5711921bfd08abc3791611fcbf017
FlyaginAS <rus.reg.36@gmail.com>
2021-10-03 23:28:09 +0300

    создал эскиз PersonInfo

--- src/components/person-info/person-info.js
+++ src/components/person-info/person-info.js
@@ -2,7 +2,30 @@ import React from 'react';
 import './person-info.css';
 
 const PersonInfo = () => {
-  return <div className="person-info">Person info</div>;
+  return (
+    <div className="person-info">
+      <h2 className="person-info_h2">Person info</h2>
+      <div className="person-info__photo-wrap">
+        <img
+          className="person-info__photo"
+          src={`https://starwars-visualguide.com/assets/img/characters/1.jpg`}
+          alt="photo"
+        />
+      </div>
+      <div className="person-info__info">
+        <p>
+          Name: Luke Skywalker <br />
+          Height: 172 <br />
+          Mass: 100kg <br />
+          Hair color: blond <br />
+          Skin color: fair <br />
+          Eye color: blue <br />
+          Birth year: 19bby <br />
+          Gender: male <br />
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PersonInfo;
