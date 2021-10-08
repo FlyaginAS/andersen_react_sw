119b82f99282502b528a6bed9d1ce953c3b984be
FlyaginAS <rus.reg.36@gmail.com>
2021-10-08 12:43:00 +0300

    начал реализовывать функциональность signin

--- src/components/pages/signin.js
+++ src/components/pages/signin.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import storageService from '../../services/storage-service';
 
 const Signin = () => {
-  const signin = () => {};
+  const { getFromStorage } = storageService;
+  const [login, setLogin] = useState();
+  const [password, setPassword] = useState();
+
+  const signin = (log, pass) => {
+    //read from localstorage
+    //check password
+    //write user  to state
+  };
+
   return (
     <div>
       <h2>Войти</h2>
       <form>
-        <input type="text" placeholder="Enter login" required />
+        <input
+          type="text"
+          placeholder="Enter login"
+          onChange={(evt) => setLogin(evt.target.value)}
+          value={login}
+          required
+        />
         <input
           type="password"
           placeholder="Enter password"
+          onChange={(evt) => setPassword(evt.target.value)}
+          value={password}
           required
         />
         <button onClick={() => signin}>Войти</button>
