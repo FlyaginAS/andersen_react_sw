84d3251e53d832012f67047cb607a62e65edc962
FlyaginAS <rus.reg.36@gmail.com>
2021-10-11 15:14:15 +0300

    отрефакторил addHistoryToStorage

--- src/middlewares/addHistoryToStorage.js
+++ src/middlewares/addHistoryToStorage.js
@@ -1,16 +1,29 @@
 import storageService from '../services/storage-service';
 
 const addHistoryToStorage = (store) => (next) => (action) => {
-  if (action.type === 'authorization/addHistory') {
-    if (store.getState().authorization.user) {
-      next(action);
-      console.log('added history to storage');
-      const user = store.getState().authorization.user;
-      storageService.setToStorage(user.login, user);
-    }
-  } else {
-    next(action);
+  if (action.type !== 'authorization/addHistory') {
+    return next(action);
   }
+
+  const user = store.getState().authorization.user;
+
+  if (!user) {
+    return;
+  }
+
+  next(action);
+  storageService.setToStorage(user.login, user);
+
+  // if (action.type === 'authorization/addHistory') {
+  //   if (store.getState().authorization.user) {
+  //     next(action);
+  //     console.log('added history to storage');
+  //     const user = store.getState().authorization.user;
+  //     storageService.setToStorage(user.login, user);
+  //   }
+  // } else {
+  //   next(action);
+  // }
 };
 
 export default addHistoryToStorage;
