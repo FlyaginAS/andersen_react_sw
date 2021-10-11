20fdf791b806d9cacef1c580d038320059ab3f13
FlyaginAS <rus.reg.36@gmail.com>
2021-10-11 14:55:32 +0300

    вернулся к рабочему состоянию после неудачного резета

--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,66 @@ import authorizationReducer, {
   logoutUser,
 } from './authorizationSlice';
 import storageService from '../services/storage-service';
-//myMiddlewares
-import {
-  logOut,
-  logIn,
-  checkPermissionToAddToFavorites,
-  addHistoryToStorage,
-} from '../middlewares';
 
+//myMiddleware
+const logOut = (store) => (next) => (action) => {
+  if (action.type === 'authorization/logoutUser') {
+    storageService.setToStorage('lastActiveUser', '');
+  }
+  next(action);
+};
+const logIn = (store) => (next) => (action) => {
+  if (action.type === 'authorization/loginUser') {
+    storageService.setToStorage(
+      'lastActiveUser',
+      action.payload.login
+    );
+  }
+  next(action);
+};
 //проверка разрешения диспатчить в избранное
+const checkPermissionToAddToFavorites =
+  (store) => (next) => (action) => {
+    if (action.type === 'authorization/addFavorites') {
+      //todo переписать условие через &
+      if (store.getState().authorization.user) {
+        if (
+          store
+            .getState()
+            .authorization.user.favorites.includes(action.payload)
+        ) {
+          alert('Персонаж уже был добавлен в избранное');
+          return;
+        }
+        next(action);
+        //сохраняем в стор обновленного юзера. Это побочное
+        //действие можно выполнять или тут, или в компоненте,
+        // но не в редюсере- он чистая ф-ия
+        const user = store.getState().authorization.user;
+        storageService.setToStorage(user.login, user);
+        alert('Персонаж добавлен');
+      } else {
+        alert(
+          'Только зарегистрированные пользователи могут добавлять в избранное'
+        );
+      }
+    } else {
+      next(action);
+    }
+  };
+
+const addHistoryToStorage = (store) => (next) => (action) => {
+  if (action.type === 'authorization/addHistory') {
+    if (store.getState().authorization.user) {
+      next(action);
+      console.log('added history to storage');
+      const user = store.getState().authorization.user;
+      storageService.setToStorage(user.login, user);
+    }
+  } else {
+    next(action);
+  }
+};
 
 const store = configureStore({
   reducer: {
